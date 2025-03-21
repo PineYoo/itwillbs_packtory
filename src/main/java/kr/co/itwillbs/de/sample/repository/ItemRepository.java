d2c96c821bb6fddcbd510301484ca15d0235a34d
mingming-01 <gjals0318@gmail.com>
2025-03-21 15:52:30 +0900

    ada

--- src/main/java/kr/co/itwillbs/de/sample/repository/ItemRepository.java
+++ src/main/java/kr/co/itwillbs/de/sample/repository/ItemRepository.java
@@ -1,6 +1,6 @@
 package kr.co.itwillbs.de.sample.repository;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
