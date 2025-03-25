71a4a84023babc965bc31fe83878c8358f593daf
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2025-03-25 18:04:09 +0900

    ggggggg

--- src/main/java/kr/co/itwillbs/de/approval/dto/ApprovalDTO.java
+++ src/main/java/kr/co/itwillbs/de/approval/dto/ApprovalDTO.java
@@ -1,29 +1,5 @@
 package kr.co.itwillbs.de.approval.dto;
 
-import java.io.File;
-
-import groovy.transform.ToString;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 public class ApprovalDTO {
-	private Long idx;
-	
-	
-	private String approval_type;		
-	
-	private String doc_code;
-	
-	private String title;
-	
-	private String content;
-	
-	
-	private File file;
-	
+
 }
