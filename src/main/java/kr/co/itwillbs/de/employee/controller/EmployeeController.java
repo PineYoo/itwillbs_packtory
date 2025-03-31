11a3a2bb79e1d773e83ef91248467a1895263877
mingming-01 <gjals0318@gmail.com>
2025-03-31 12:51:47 +0900

    Employee_detail 작업 주석 남기겠습니다.

--- src/main/java/kr/co/itwillbs/de/employee/controller/EmployeeController.java
+++ src/main/java/kr/co/itwillbs/de/employee/controller/EmployeeController.java
@@ -99,6 +99,16 @@ public class EmployeeController {
     	return ResponseEntity.ok("삭제되었습니다.");
     }
 
+    /*
+     * =============================== 여기서 부터 문제 ===============================
+     * 현재 상세 페이지 불러올 때 employee_detail 정보가 없기 때문에 콘솔창에 오류가 나고 있어요.
+     * 빈 페이지라도 불러오는 로직을 추가해뒀는데 계속해서 오류가 나는 이유를 모르겠어요.
+     * 현제 디테일 뷰 페이지를 보면 사원 수정 버튼을 눌렀을 때 readonly가 풀리면서 입력폼으로 변경 됩니다.
+     * 이렇게 저장 버튼을 누르면 셀렉or수정 작업을 완료하고 t_employee_detail 디비에도 값이 저장되어야 함.
+     * 
+     * 기존에는 디테일 정보 수정에서 카카오 API를 넣거나, 이메일 도메인을 드롭다운 방식으로 보여줘서 실제 회원가입 동작 처럼
+     * 만들고 싶었지만 시간 관계상 포기해야 할것 같습니다.
+     */
     // 사원 상세정보 페이지
     @GetMapping("/detail/{id}")
     public String showEmployeeDetail(@PathVariable("id") String id, Model model) {
@@ -120,5 +130,7 @@ public class EmployeeController {
 
         return "redirect:/employee/detail/" + id;
     }
-
+    /*
+     * =============================== 여기 까지 문제 ===============================
+     */
 }
