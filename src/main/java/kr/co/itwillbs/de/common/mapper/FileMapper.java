0da50e1514033d88984055f66b739938c004c176
PineYoo <pine01090757260@gmail.com>
2025-04-01 15:21:48 +0900

    그룹단위 파일 가져오는 것 type 추가

--- src/main/java/kr/co/itwillbs/de/common/mapper/FileMapper.java
+++ src/main/java/kr/co/itwillbs/de/common/mapper/FileMapper.java
@@ -3,6 +3,7 @@ package kr.co.itwillbs.de.common.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import kr.co.itwillbs.de.common.vo.FileVO;
 
@@ -15,7 +16,8 @@ public interface FileMapper {
 	
 	FileVO getFileByIdx(long idx);
 
-	List<FileVO> getFilesByMajorIdx(long majorIdx);
+	List<FileVO> getFilesByMajorIdx(@Param("type")String type, 
+									@Param("majorIdx")long majorIdx);
 
 
 }
--- src/main/java/kr/co/itwillbs/de/common/service/FileService.java
+++ src/main/java/kr/co/itwillbs/de/common/service/FileService.java
@@ -1,10 +1,23 @@
 package kr.co.itwillbs.de.common.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import kr.co.itwillbs.de.common.mapper.FileMapper;
+import kr.co.itwillbs.de.common.util.FileUtil;
 import kr.co.itwillbs.de.common.vo.FileVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,9 +26,63 @@ import lombok.extern.slf4j.Slf4j;
 public class FileService {
 
 	private final FileMapper fileMapper;
+	private final FileUtil fileUtil;
 	
-	public FileService(FileMapper fileMapper) {
+	public FileService(FileMapper fileMapper, FileUtil fileUtil) {
 		this.fileMapper = fileMapper;
+		this.fileUtil = fileUtil;
+	}
+	
+	/**
+	 * 
+	 * @param fileIdx
+	 * @return
+	 */
+	public ResponseEntity<Resource> getFile(Long fileIdx) throws ResponseStatusException {
+		log.info("{}---start", Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		// fileIdx로 파일 정보 가져오기
+		FileVO fileVO = fileMapper.getFileByIdx(fileIdx);
+		
+		// 다운로드 할 파일 정보를 서버 상의 업로드 디렉토리에 접근하여 가져오기
+		String strPath = FileUtil.UPLOAD_DIR+fileVO.getFilePath()+fileVO.getFileName();
+		
+		// 업로드/다운로드 작업 전 파일 경로 검증 메서드
+		Path uploadPath = Paths.get(FileUtil.chekcFileSeparator(strPath));
+		
+		// 파일 경로 + 파일 명 더하기!
+		Path path = Paths.get(uploadPath.toString())
+					.resolve(fileVO.getFileName()).normalize();
+		// UrlResource 객체를 생성하여 이미지 파일에 대한 리소스 객체 생성 => Resource 타입으로 업캐스팅
+		try {
+			Resource resource = new UrlResource(path.toUri());
+			log.info("path {}, path.toUri {}", path, path.toUri());
+			
+			// 경로 존재 여부 판별 및 파일 접근 가능 여부 확인
+			if(!resource.exists() || !resource.isReadable()) {
+				log.info("!resource.exists() || !resource.isReadable() were true!");
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다.");
+			}
+			
+			// 파일의 MIME 타입(컨텐츠 타입) 설정
+			// 실제 파일로부터 컨텐츠 타입 알아내기
+			String contentType = Files.probeContentType(path);
+			if(contentType == null) { // 컨텐츠 타입 조회 실패 시
+				log.info("contentType == null => application/octet-stream");
+				contentType = "application/octet-stream";
+			}
+			
+			// 응답 데이터(ResponseEntity(Resource> 타입) 리턴
+			return ResponseEntity.ok() // 정상적인 응답(HTTP_OK = 200)으로 설정
+					.contentType(MediaType.parseMediaType(contentType)) // 자동으로 설정된 컨텐츠 타입으로 설정 이미지 파일은 브라우저에서 직접 열리고, 그 외의 대부분의 파일은 다운로드 창 열림
+					//.contentType(MediaType.APPLICATION_OCTET_STREAM) // 자동으로 설정된 컨텐츠 타입으로 설정 이미지 파일은 브라우저에서 직접 열리고, 그 외의 대부분의 파일은 다운로드 창 열림
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+ fileVO.getFileName() + "\"")
+					.body(resource);
+		} catch (MalformedURLException e) { 
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 다운로드 실패");
+		} catch (IOException e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 타입 확인 실패");
+		}
 	}
 	
 	/**
@@ -25,8 +92,6 @@ public class FileService {
 	 */
 	public int registerFile(FileVO fileVO) {
 		log.info("{}---start", Thread.currentThread().getStackTrace()[1].getMethodName());
-		
-		
 		
 		return fileMapper.registerFile(fileVO);
 	}
@@ -62,10 +127,10 @@ public class FileService {
 	 * @param idx
 	 * @return
 	 */
-	public List<FileVO> getFilesByMajorIdx(long majorIdx) {
+	public List<FileVO> getFilesByMajorIdx(String type, long majorIdx) {
 		log.info("{}---start", Thread.currentThread().getStackTrace()[1].getMethodName());
 		
-		List<FileVO> fileList = fileMapper.getFilesByMajorIdx(majorIdx);
+		List<FileVO> fileList = fileMapper.getFilesByMajorIdx(type, majorIdx);
 		
 		return fileList;
 	}
