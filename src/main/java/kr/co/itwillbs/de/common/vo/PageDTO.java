b32b70e6c2ef59c0c7e4e01fa71d91cddeb1a85d
pine0 <pine0@host.docker.internal>
2025-04-20 14:32:33 +0900

    JPA 페이징 용 메서드 추가

--- src/main/java/kr/co/itwillbs/de/common/vo/PageDTO.java
+++ src/main/java/kr/co/itwillbs/de/common/vo/PageDTO.java
@@ -1,5 +1,9 @@
 package kr.co.itwillbs.de.common.vo;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,20 +12,20 @@ import lombok.ToString;
 @Setter
 @ToString
 public class PageDTO {
-	
+
 	// 페이징
 	private int page = 1; // 기본 1페이지
 	private int size = 10; // 기본 10개씩
 	private int totalCount = 0;
-	
+
 	// groupSize는 화면에 보여줄 페이지 수 (예: 5개씩)
 	private final int groupSize = 5;
-	
+
 	// 계산 값
 	public int getOffset() {
 		return (page - 1) * size;
 	}
-	
+
 	public int getTotalPage() {
 		return (int) Math.ceil((double) totalCount / size);
 	}
@@ -41,5 +45,10 @@ public class PageDTO {
 
 	public boolean isHasNextGroup() {
 		return getEndPageGroup() < getTotalPage();
+	}
+
+	// JPA 페이징 메서드 추가
+	public Pageable toPageable(Sort sort) {
+		return PageRequest.of(this.page - 1, this.size, sort);
 	}
 }
