c7b1cc0d968c222e7bfba630ac17002a56c93669
mingming-01 <gjals0318@gmail.com>
2025-04-25 11:43:49 +0900

    rawmaterial 일단 커밋.. detail은 다음주에....

--- src/main/java/kr/co/itwillbs/de/mes/service/RawMaterialService.java
+++ src/main/java/kr/co/itwillbs/de/mes/service/RawMaterialService.java
@@ -17,51 +17,69 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RawMaterialService {
 
-	private final RawMaterialMapper rawMaterialMapper; // 원자재 관련 Mapper 주입
+	private final RawMaterialMapper rawMaterialMapper; // 마스터 자재 관련 Mapper 주입
 
-	// 원자재 등록
+	// 마스터 자재 등록
 	@LogExecution
 	@Transactional
-	public void registerRawMaterial(RawMaterialDTO rawMaterialDTO) {
-		log.info("원자재 등록 요청: {}", rawMaterialDTO);
-		rawMaterialMapper.insertRawMaterial(rawMaterialDTO);
-		log.info("원자재 등록 완료 - name: {}", rawMaterialDTO.getName());
+	public void registerMasterMaterial(RawMaterialDTO rawMaterialDTO) {
+		log.info("마스터 자재 등록 요청: {}", rawMaterialDTO);
+		rawMaterialMapper.insertMasterMaterial(rawMaterialDTO);
+		log.info("마스터 자재 등록 완료 - name: {}", rawMaterialDTO.getName());
 	}
 
-	// 원자재 총 개수 (검색 조건 포함)
-	public int searchRawMaterialCount(RawMaterialSearchDTO searchDTO) {
-		log.info("원자재 개수 조회 - 검색 조건: {}", searchDTO);
-		return rawMaterialMapper.searchRawMaterialCount(searchDTO);
+	// 마스터 자재 조회 + 페이징
+	public int searchMasterMaterialCount(RawMaterialSearchDTO searchDTO) {
+		log.info("마스터 자재 개수 조회 - 검색 조건: {}", searchDTO);
+		return rawMaterialMapper.searchMasterMaterialCount(searchDTO);
 	}
 
-	// 원자재 목록 조회 (검색 + 페이징)
-	public List<RawMaterialDTO> getRawMaterialList(RawMaterialSearchDTO searchDTO) {
-		log.info("원자재 목록 조회 - 검색 조건: {}", searchDTO);
-		return rawMaterialMapper.searchRawMaterial(searchDTO);
+	// 마스터 자재 조회
+	public List<RawMaterialDTO> getMasterMaterialList(RawMaterialSearchDTO searchDTO) {
+		log.info("마스터 자재 목록 조회 - 검색 조건: {}", searchDTO);
+		return rawMaterialMapper.searchMasterMaterial(searchDTO);
 	}
 
-	// 원자재 상세 조회
-	public RawMaterialDTO getRawMaterial(Long idx) {
-		log.info("원자재 상세 조회 - idx: {}", idx);
-		return rawMaterialMapper.getRawMaterialByIdx(idx);
+	// 마스터 자재 상세 조회
+	public RawMaterialDTO getMasterMaterial(Long idx) {
+		log.info("마스터 자재 상세 조회 - idx: {}", idx);
+		return rawMaterialMapper.getMasterMaterialByIdx(idx);
 	}
 
-	// 원자재 수정
+	// 마스터 자재 정보 수정
 	@LogExecution
 	@Transactional
-	public void updateRawMaterial(RawMaterialDTO rawMaterialDTO) {
-		log.info("창고 수정 요청 - idx: {}", rawMaterialDTO.getIdx());
-
-        // 원자재가 null이 아닌지 체크
+	public void updateMasterMaterial(RawMaterialDTO rawMaterialDTO) {
+		log.info("마스터 자재 수정 요청 - idx: {}", rawMaterialDTO.getIdx());
+        // 마스터 자재가 null이 아닌지 체크
         if (rawMaterialDTO != null) {
-            // 원자재 정보를 업데이트하는 쿼리 호출
-        	rawMaterialMapper.updateRawMaterial(rawMaterialDTO);
-            log.info("창고 수정 완료 - name: {}", rawMaterialDTO.getName());
+            // 마스터 자재 정보를 업데이트하는 쿼리 호출
+        	rawMaterialMapper.updateMasterMaterial(rawMaterialDTO);
+            log.info("마스터 자재 수정 완료 - name: {}", rawMaterialDTO.getName());
         } 
     }
-
-	// 원자재 정보 담아 가기 (외부용)
-	public  List<RawMaterialDTO> getRawMaterialList() {
+	
+	// 부속 자재 조회
+	public List<RawMaterialDTO> getSubMaterialsByIdx(Long idx) {
+		return rawMaterialMapper.getSubMaterialsByIdx(idx);
+	}
+	
+	// 부속 자재 등록
+	@LogExecution
+	@Transactional
+	public void insertSubMaterial(RawMaterialDTO rawMaterialDTO) {
+		rawMaterialMapper.insertSubMaterial(rawMaterialDTO);
+	}
+	
+	// 부속 자재 정보 수정
+	@LogExecution
+	@Transactional
+	public void updateSubMaterial(RawMaterialDTO rawMaterialDTO) {
+		rawMaterialMapper.updateSubMaterial(rawMaterialDTO);
+	}
+	
+	// 자재 정보 담아 가기 (외부용)
+	public List<RawMaterialDTO> getRawMaterialList() {
 		return rawMaterialMapper.selectRawMaterialList();
 	}
 }
