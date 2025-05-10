653b42ffde48d2e1727d81c38f39e7c56bababcf
pine0 <pine0@host.docker.internal>
2025-05-10 23:41:39 +0900

    packtory 프로젝트 종료 후 설정들 정리하기 1) 프로퍼티 암호화

--- src/main/java/kr/co/itwillbs/de/common/config/EnvPropertyConfig.java
+++ src/main/java/kr/co/itwillbs/de/common/config/EnvPropertyConfig.java
@@ -1,25 +1,86 @@
 package kr.co.itwillbs.de.common.config;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertySourceFactory;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.core.io.ClassPathResource;
 
-@PropertySource(value = "/packtory/env/env.yml", factory = EnvPropertyConfig.class)
-@ConfigurationProperties
-public class EnvPropertyConfig implements PropertySourceFactory{
-	
-	@Override
-	public PropertiesPropertySource createPropertySource(String name, EncodedResource encodedResource) throws IOException {
-		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-		factory.setResources(encodedResource.getResource());
-		Properties properties = factory.getObject();
+/**
+ * <pre>
+ * 리눅스 환경에서는 /home/projectName/env/env.properties or 프로젝트 파티션 경로/env/env.properties 에 둘 것이고
+ * 윈도우에서는 classpath:/env/env.properties 에 두고 작업을 하는 걸로!
+ * 다만, 주의 해야 할 점은 evn.properties 파일은 반드시 git에 ignore가 되어 있어야 한다.
+ * 
+ * - 추가
+ * 규모가 큰 프로젝트의 경우 역할이나 성질에 따라 foo.properties, bar.properties 로 구분할텐데
+ * 작은 규모의 프로젝트에서는 env(환경).properties 라는 걸 만들어두고
+ * 추후에 사용될 수 있는 api키나 token 값들도 저장해서 부트가 기동될 때 자바프로퍼티로 불러와서 사용해야 할텐데
+ * 이정도에서 멈추는게 맞나...?
+ * - 추가2
+ * @ConfigurationProperties(prefix = "blabla") 아무거나 입력 할것 같아서 통일시킬게 없을 것 같다.
+ * public class EnvProperties { 필드 입력}
+ * 식의 클래스로 부트스럽게 활용할 수 있을 것 같다.
+ * </pre>
+ */
+public class EnvPropertyConfig {
 
-		return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+	private static final String ENV_FILE_PATH = "env/env.properties";
+
+	public static Properties load() {
+		Properties props = new Properties();
+		try (InputStream input = new ClassPathResource(ENV_FILE_PATH).getInputStream()) {
+			props.load(input);
+		} catch (IOException e) {
+			throw new IllegalStateException("env.properties not found env file: " + ENV_FILE_PATH, e);
+		}
+		return props;
+	}
+
+	/**
+	 * 암호화 테스트
+	 * @param args
+	 */
+	public static void main(String... args) {
+		// 0) 암호화할 플래인Str 셋
+		List<String> plainStrs = new ArrayList<>();
+		plainStrs.add("com.mysql.cj.jdbc.Driver");
+		
+		// 1) env.properties 로딩
+		Properties secrets = new Properties();
+		try (InputStream in = new ClassPathResource(ENV_FILE_PATH).getInputStream()) {
+			secrets.load(in);
+		} catch (Exception e) {
+			System.err.println("env properties 로드 실패: " + e.getMessage());
+			System.exit(2);
+		}
+
+		// 2) Jasypt Encryptor 구성
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(secrets.getProperty("encryptor.password"));
+		System.out.println(secrets.getProperty("encryptor.password"));
+		config.setAlgorithm(secrets.getProperty("encryptor.algorithm", "PBEWITHHMACSHA512ANDAES_256"));
+		System.out.println(secrets.getProperty("encryptor.algorithm"));
+		config.setKeyObtentionIterations(secrets.getProperty("encryptor.iterations", "2000"));
+		config.setPoolSize(secrets.getProperty("encryptor.pool-size", "1"));
+		config.setSaltGeneratorClassName(
+				secrets.getProperty("encryptor.salt-generator", "org.jasypt.salt.RandomSaltGenerator"));
+		// iv-generator는 AES 알고리즘 쓸 때만 필요하므로, env 파일에 없으면 생략합니다.
+		config.setIvGeneratorClassName(
+				secrets.getProperty("encryptor.iv-generator", "org.jasypt.iv.RandomIvGenerator"));
+		config.setStringOutputType("base64");
+
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		encryptor.setConfig(config);
+
+		// 3) 암호화 & 출력
+		for (String string : plainStrs) {
+			String encrypted = encryptor.encrypt(string);
+			System.out.println("ENC(" + encrypted + ")");
+		}
 	}
 }
---
+++ src/main/java/kr/co/itwillbs/de/common/config/JasyptEncryptorConfig.java
@@ -0,0 +1,41 @@
+package kr.co.itwillbs.de.common.config;
+
+import java.util.Properties;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+/**
+ * application.yml 암호화 된 값을 복호화 하기 위한 클래스
+ */
+@Configuration
+@EnableEncryptableProperties
+public class JasyptEncryptorConfig {
+
+	private final String PROPERTY_PASSWORD = "encryptor.password";
+	private final String PROPERTY_ALGORITHM = "encryptor.algorithm";
+	//private final String PROPERTY_SLAT = "encryptor.password";
+	
+	@Bean(name = "jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		Properties secrets = EnvPropertyConfig.load(); // 이렇게 가져와서 쓰는거면.. 그냥 이 클래스 밑에 메서드가 낫지 않나? 고민이다.
+
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(secrets.getProperty(PROPERTY_PASSWORD));
+		config.setAlgorithm(secrets.getProperty(PROPERTY_ALGORITHM, "PBEWITHHMACSHA512ANDAES_256"));
+		config.setKeyObtentionIterations("2000");
+		config.setPoolSize("1");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+		config.setStringOutputType("base64");
+
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+}
